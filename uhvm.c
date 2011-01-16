9fcf0a4b907501aabea638aa8b95ab3777caac60
Marc Jessome <marc.jessome@gmail.com>
2011-01-16 02:03:00 -0500

    Free device if it is already mounted.

--- uhvm.c
+++ uhvm.c
@@ -349,8 +349,10 @@ device_added(LibHalContext *context, const char *did)
     if (!mountp)
         goto out;
     device = get_device(mountp, did, dev, label, fstype, volume, drive);
-    if(is_mounted(device))
+    if(is_mounted(device)) {
+        free_device(device);
         goto out;
+    }
     if (!device)
         goto out;
     consider_fstab(device);
